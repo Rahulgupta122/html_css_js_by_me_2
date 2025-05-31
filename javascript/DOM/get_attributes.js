abfd695202b348e146dda7fe7ec8ca4ccd13c12d
Rahul Gupta <122372097+Rahulgupta122@users.noreply.github.com>
2025-05-31 21:09:59 +0530

    codes

---
+++ javascript/DOM/get_attributes.js
@@ -0,0 +1,3 @@
+let a=document.querySelector("p");
+let b=a.getAttribute('id');
+console.log(b);
---
+++ javascript/DOM/setattribute.js
@@ -0,0 +1,3 @@
+let a=document.querySelector("p");
+let b=a.setAttribute("class","king");
+console.log(b);
